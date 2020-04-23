c00ffed8033fe378762f854d9f63661ced769e52
jakub-drewniak <drewniak.jakub@gmail.com>
2020-04-23 18:12:11 +0200

    Printing board component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PrintingBoardComponent } from './printing-board/printing-board.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '**', component: PrintingBoardComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/printing-board/printing-board.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-printing-board',
+  templateUrl: './printing-board.component.html',
+  styleUrls: ['./printing-board.component.scss']
+})
+export class PrintingBoardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
